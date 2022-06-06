0685b887a51db4c5087378a1d4979e5f7ec48c7b
SabbirSami <smd71430@gmail.com>
2022-06-06 17:01:54 +0600

    added custom link and fix navbar

--- src/Components/HeaderNavbar.js
+++ src/Components/HeaderNavbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/logos/logo.png";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import CustomLink from "../Shared/CustomLink";
 
 const HeaderNavbar = () => {
     return (
@@ -16,10 +17,12 @@ const HeaderNavbar = () => {
 
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto my-2 my-lg-0 ms-3">
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Our Portfolio</Nav.Link>
-                        <Nav.Link href="#action2">Our Team</Nav.Link>
-                        <Nav.Link href="#action2">Contact Us</Nav.Link>
+                        <CustomLink to="/">Home</CustomLink>
+                        <CustomLink to="/our-portfolio">
+                            Our Portfolio
+                        </CustomLink>
+                        <CustomLink to="/our-team">Our Team</CustomLink>
+                        <CustomLink to="/contact">Contact Us</CustomLink>
                     </Nav>
                     <button className="btn shadow-sm border-0 rounded-pill px-4 py-2 alert-success text-dark">
                         Login
