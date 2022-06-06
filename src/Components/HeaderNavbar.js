416a11823537f00ff42fa2c178330581801c1688
SabbirSami <smd71430@gmail.com>
2022-06-06 16:53:19 +0600

    navbar done

--- src/Components/HeaderNavbar.js
+++ src/Components/HeaderNavbar.js
@@ -5,7 +5,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const HeaderNavbar = () => {
     return (
         <Navbar bg="light" className="py-4" expand="lg">
-            <Container className=" ">
+            <Container className="justify-content-between mx-auto ">
                 <div className="d-flex justify-content-between align-items-center">
                     <img className="w-25" src={logo} alt="" />
                     <Navbar.Toggle
@@ -15,13 +15,13 @@ const HeaderNavbar = () => {
                 </div>
 
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto my-2 my-lg-0">
+                    <Nav className="me-auto my-2 my-lg-0 ms-3">
                         <Nav.Link href="#action1">Home</Nav.Link>
                         <Nav.Link href="#action2">Our Portfolio</Nav.Link>
                         <Nav.Link href="#action2">Our Team</Nav.Link>
                         <Nav.Link href="#action2">Contact Us</Nav.Link>
                     </Nav>
-                    <button className="btn shadow-sm border-0 rounded-pill px-4 py-2 alert-success">
+                    <button className="btn shadow-sm border-0 rounded-pill px-4 py-2 alert-success text-dark">
                         Login
                     </button>
                 </Navbar.Collapse>
