efcbe3d6791ab3656eae325617697aea7cbb097e
SabbirSami <smd71430@gmail.com>
2022-06-25 13:06:48 +0600

    fully login from design done

--- src/Shared/Login.js
+++ src/Shared/Login.js
@@ -1,21 +1,26 @@
 import React from "react";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Button from "react-bootstrap/Button";
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineGithub } from "react-icons/ai";
+import { GrFacebookOption } from "react-icons/gr";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
+        <div
+        // className="d-flex align-items-center justify-content-center"
+        // style={{ height: "75vh" }}
+        >
             <div className="container">
-                <div className="py-4 text-center">
-                    <h1 className="fw-bold fs-0-5">Log In</h1>
+                <div className="pt-4 text-center">
+                    <h1 className="fw-bold fs-0-5 pt-lg-5 pb-2">Log In</h1>
                     <p>
                         Don't have and account?{" "}
                         <Link to={"/sing-up"}>Sign Up</Link>
                     </p>
                 </div>
-                <div className="row">
+                <div className="row align-items-center">
                     <div className="col-lg-6 px-lg-5">
                         <div className="p-5">
                             <Form>
@@ -66,6 +71,51 @@ const Login = () => {
                             </Form>
                         </div>
                     </div>
+                    <div className="col-lg-6 px-lg-5 border-start">
+                        <div className="p-5">
+                            <button className="p-0 btn btn-primary mb-3 rounded-0">
+                                <div className="d-flex align-items-center pe-4">
+                                    <div className="bg-white p-1">
+                                        <FcGoogle className="fs-1" />
+                                    </div>
+                                    <p className="m-0 px-5">
+                                        Continue with Google
+                                    </p>
+                                </div>
+                            </button>
+                            <button className="p-0 btn btn-light mb-3 border border-dark rounded-0">
+                                <div className="d-flex align-items-center">
+                                    <div className="bg-primary p-1">
+                                        <GrFacebookOption className="fs-1 text-white" />
+                                    </div>
+                                    <p className="m-0 px-5">
+                                        Continue with Facebook
+                                    </p>
+                                </div>
+                            </button>
+                            <button className="p-0 btn btn-outline-dark mb-3 rounded-0">
+                                <div className="d-flex align-items-center pe-4">
+                                    <div className="bg-white p-1 border-end border-dark">
+                                        <AiOutlineGithub className="fs-1 " />
+                                    </div>
+                                    <p className="m-0 px-5">
+                                        Continue with GitHub
+                                    </p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-6 mx-auto">
+                    <p className="text-center mx-auto text-muted">
+                        <small>
+                            * By Logging in, you agree to our{" "}
+                            <span className="text-primary">Terms of Use</span>{" "}
+                            and to receive Creative Agency emails & updates and
+                            acknowledge that you read our{" "}
+                            <span className="text-primary">Privacy Policy</span>
+                        </small>
+                    </p>
                 </div>
             </div>
         </div>
