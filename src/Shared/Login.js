d6bb58e49481ffd0301a2a5694a7a74d70492fd7
SabbirSami <smd71430@gmail.com>
2022-06-25 12:02:39 +0600

    firebase and react firebase package added

--- src/Shared/Login.js
+++ src/Shared/Login.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+    return <div></div>;
 };
 
 export default Login;
