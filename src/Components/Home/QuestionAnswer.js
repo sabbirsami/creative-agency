d301b9737f6633170aa1487d381b51286128c04e
SabbirSami <smd71430@gmail.com>
2022-06-28 23:18:11 +0600

    Question answer section done

--- src/Components/Home/QuestionAnswer.js
+++ src/Components/Home/QuestionAnswer.js
@@ -13,20 +13,24 @@ const QuestionAnswer = () => {
                                 eventKey="0"
                             >
                                 <Accordion.Header className="border">
-                                    <p className="pt-2"> Accordion Item #1</p>
+                                    <p className="pt-2">
+                                        How will you improve the performance of
+                                        a React Application?
+                                    </p>
                                 </Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip
-                                    ex ea commodo consequat. Duis aute irure
-                                    dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt
-                                    mollit anim id est laborum.
+                                    A React application may be made more
+                                    efficient in a variety of ways. As an
+                                    example, wherever possible, keep state of
+                                    components local. Memorizing React
+                                    components to avoid unwanted re-renders is
+                                    also a good strategy. In addition, dynamic
+                                    imports in React allow for code-splitting
+                                    (). React's virtualization of windows or
+                                    lists. In React, photos may be loaded in a
+                                    'lazy' manner. Making Use of Structural
+                                    Consistency. As many chunk files as you'd
+                                    want.
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item
@@ -34,20 +38,59 @@ const QuestionAnswer = () => {
                                 eventKey="1"
                             >
                                 <Accordion.Header className="border">
-                                    <p className="pt-2"> Accordion Item #1</p>
+                                    <p className="pt-2">
+                                        Explain Call by value vs call by
+                                        reference
+                                    </p>
                                 </Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip
-                                    ex ea commodo consequat. Duis aute irure
-                                    dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt
-                                    mollit anim id est laborum.
+                                    We pass the values of variables while
+                                    calling a function. On the other hand,
+                                    instead of passing values of variables, we
+                                    pass addresses of variables. In
+                                    call-by-values, we cannot alter the values
+                                    of actual variables through function calls.
+                                    On the other hand, in the call by reference,
+                                    we can alter the values of variables through
+                                    function calls.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item
+                                className="border-0 py-1"
+                                eventKey="1"
+                            >
+                                <Accordion.Header className="border">
+                                    <p className="pt-2">
+                                        What are the different ways to manage a
+                                        state in a React application?
+                                    </p>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    The four major forms of state you need to
+                                    keep track of in your React projects are
+                                    Local state, Global state, Server state, and
+                                    URL state. A component's local state refers
+                                    to the data it keeps. Managing local state
+                                    in React is often accomplished via the usage
+                                    of the useState hook. Additionally, for both
+                                    local and global states, useReducer is an
+                                    option that may be implemented in any way.
+                                    Under the hood, it is quite similar to
+                                    useState, except that it takes a reducer
+                                    instead of a starting state. You'd need a
+                                    local state for things like showing or
+                                    hiding a modal component and keeping track
+                                    of input data for forms that have been
+                                    disabled. Data that is shared across
+                                    different components is referred to as a
+                                    global state. When data has to be accessed
+                                    and updated across many parts of our
+                                    program, we need to use a global state.
+                                    Authenticated user state is a frequent
+                                    example of a global state. To get or update
+                                    data from an external server, you must
+                                    handle a variety of states, including
+                                    loading and error state.
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item
@@ -55,20 +98,17 @@ const QuestionAnswer = () => {
                                 eventKey="2"
                             >
                                 <Accordion.Header className="border">
-                                    <p className="pt-2"> Accordion Item #1</p>
+                                    <p className="pt-2">
+                                        What is the scope of JavaScript?
+                                    </p>
                                 </Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip
-                                    ex ea commodo consequat. Duis aute irure
-                                    dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt
-                                    mollit anim id est laborum.
+                                    The difference between local storage and
+                                    session storage is local storage stores data
+                                    if you reload the page or relaunch the
+                                    browser data will still save but on the
+                                    other hand, session storage data is
+                                    temporary.
                                 </Accordion.Body>
                             </Accordion.Item>
                             <Accordion.Item
@@ -76,20 +116,19 @@ const QuestionAnswer = () => {
                                 eventKey="3"
                             >
                                 <Accordion.Header className="border">
-                                    <p className="pt-2"> Accordion Item #1</p>
+                                    <p className="pt-2">
+                                        Difference between Array vs LinkedList.
+                                    </p>
                                 </Accordion.Header>
                                 <Accordion.Body>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud
-                                    exercitation ullamco laboris nisi ut aliquip
-                                    ex ea commodo consequat. Duis aute irure
-                                    dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt
-                                    mollit anim id est laborum.
+                                    An array is a collection of elements of a
+                                    similar data type. On the other hand, Linked
+                                    list is an ordered collection of elements of
+                                    the same type in which each element is
+                                    connected to the next using pointers. Array
+                                    elements are not dependent on each other on
+                                    the other hand linked list elements are
+                                    dependent on each other.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
